d6563498f4e5c4346c89c52c1d76327e310b3b00
Guillaume ROUSSIN <guillaume.roussin44@gmail.com>
2021-01-06 21:24:46 +0000

    fixed Nezuko

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
-const dbm = require('./packages/DatabaseManager');
 
 const Discord = require('discord.js');
 const {prefix, token, adminRole} = require('./config.json');
 
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-const db = new dbm.DatabaseManager(__dirname+'/models/');
 
 // Loading commands
 bot.commands = new Discord.Collection();
